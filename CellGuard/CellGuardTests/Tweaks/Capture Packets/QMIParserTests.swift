3842f8af818273878dc36f5b0646285118ca75a3
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2023-06-17 12:29:53 +0200

    Adds a QMI packet test with an zero-length TLV

--- CellGuard/CellGuardTests/Tweaks/Capture Packets/QMIParserTests.swift
+++ CellGuard/CellGuardTests/Tweaks/Capture Packets/QMIParserTests.swift
@@ -40,6 +40,35 @@ final class QMIParserTests: XCTestCase {
         XCTAssertEqual(packet.tlvs[1].data, Data([0x30, 0x0C]))
     }
     
+    func testCTLPacketWithEmptyTLV() throws {
+        // Some packet collected during testing
+        let data = Data(base64Encoded: "ARYAAAAAAJkpAAsAAQUAAQAAAAQRAAA=")!
+        let packet = try ParsedQMIPacket(nsData: data)
+        
+        XCTAssertEqual(packet.qmuxHeader.length, 22)
+        XCTAssertEqual(packet.qmuxHeader.flag, 0x00)
+        XCTAssertEqual(packet.qmuxHeader.serviceId, 0x00)
+        XCTAssertEqual(packet.qmuxHeader.clientId, 0x00)
+        
+        XCTAssertEqual(packet.transactionHeader.compound, false)
+        XCTAssertEqual(packet.transactionHeader.response, false)
+        XCTAssertEqual(packet.transactionHeader.indication, false)
+        XCTAssertEqual(packet.transactionHeader.transactionId, 0x99)
+        
+        XCTAssertEqual(packet.messageHeader.messageId, 0x0029)
+        XCTAssertEqual(packet.messageHeader.messageLength, 11)
+        
+        XCTAssertEqual(packet.tlvs.count, 2)
+        
+        XCTAssertEqual(packet.tlvs[0].type, 0x01)
+        XCTAssertEqual(packet.tlvs[0].length, 5)
+        XCTAssertEqual(packet.tlvs[0].data, Data([0x01, 0x00, 0x00, 0x00, 0x04]))
+        
+        XCTAssertEqual(packet.tlvs[1].type, 0x11)
+        XCTAssertEqual(packet.tlvs[1].length, 0)
+        XCTAssertEqual(packet.tlvs[1].data, Data(count: 0))
+    }
+    
     func testNASPacket() throws {
         // 1702th packet from a private trace
         let data = Data(base64Encoded: "AbYAgAMBBBYATgCqABACAAAAEQIAAAASAwAAAAATAwAAAAAUAwACAgAZHQABAwEDAQABAAD//wEDF+8AAAAAATI2MjAy/wF0tR4CAP//IQEAAScBAAAoBAABAAAAKgEAASsEAAEAAAAwBAAAAAAAMgQAAAAAADUCAP//OQQAAQAAADoEAAEAAAA/BAAAAAAARQQAAwAAAEcEAAQAAABMAwAAAABQAQABUQEAAFcBAAFdBAAAAAAA")!
