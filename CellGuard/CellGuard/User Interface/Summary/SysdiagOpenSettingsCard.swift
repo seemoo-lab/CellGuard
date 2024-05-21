11b7759633789595b13883a81305d96498279ce6
jiska <jclassen@seemoo.tu-darmstadt.de>
2024-05-21 23:01:49 +0200

    fix for jailbreak build error

--- CellGuard/CellGuard/User Interface/Summary/SysdiagOpenSettingsCard.swift
+++ CellGuard/CellGuard/User Interface/Summary/SysdiagOpenSettingsCard.swift
@@ -15,7 +15,11 @@ struct SysdiagOpenSettingsCard: View {
     var body: some View {
         
         NavigationLink(isActive: $showingSettingsInstructions) {
+            // TODO: super weird class not found error in jailbreak case...
+            #if JAILBREAK
+            #else
             SysdiagOpenSettingsDetailedView()
+            #endif
         } label: {
             EmptyView()
         }
