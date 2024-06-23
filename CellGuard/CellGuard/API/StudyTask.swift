13a50af53c15c1f93b6bfcdbcd4a3deb8af382e5
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2024-06-23 21:21:57 +0200

    Skips StudyTask if user is not early adopter

--- CellGuard/CellGuard/API/StudyTask.swift
+++ CellGuard/CellGuard/API/StudyTask.swift
@@ -20,6 +20,11 @@ struct StudyTask {
             return
         }
         
+        // TODO: Remove to begin study
+        if !UserDefaults.standard.bool(forKey: UserDefaultsKeys.studyEarlyAdopter.rawValue) {
+            return
+        }
+        
         // Query all cells relevant for the study which have not been uploaded
         let studyCells = try persistence.fetchStudyUploadCells(startDate: participationSince)
         if !studyCells.isEmpty {
@@ -32,6 +37,8 @@ struct StudyTask {
         // - Check if the study participation was activated before the last week
         // --> Do we want to use fixed intervals (from Monday UTC to Sunday UTC) or intervals based on the activation of the switch?
         // - Generate weekly report from DB
+        // -> Only if at least one cell is present
+        // -> Date + randomness
     }
     
 }
