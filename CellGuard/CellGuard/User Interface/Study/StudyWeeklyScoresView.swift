78a4bb993e9c5a2c43fb9f2ed55a0c739ab75666
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2024-09-01 17:37:07 +0200

    Groups weekly score contributions by week & shows message if there are none

--- CellGuard/CellGuard/User Interface/Study/StudyWeeklyScoresView.swift
+++ CellGuard/CellGuard/User Interface/Study/StudyWeeklyScoresView.swift
@@ -15,30 +15,49 @@ struct StudyWeeklyScoresView: View {
     )
     private var scores: FetchedResults<StudyScore>
     
-    // TODO: Group by week
-    // TODO: Show message if so far no data has been transmitted
-    
     var body: some View {
-        return List(scores) { score in
-            Text("""
-            Week: \(weekString(score: score))
-            Country: \(score.country ?? "n/a")
-            Anomalous Cells: \(percentNumberFormatter.string(for: score.rateSuspicious) ?? "n/a")
-            Suspicious Cells: \(percentNumberFormatter.string(for: score.rateUntrusted) ?? "n/a")
-            """)
+        if !scores.isEmpty {
+            let weeklyScores = Dictionary(grouping: scores, by: { $0.week })
+                .sorted(by: { $0.key ?? Date.distantPast < $1.key ?? Date.distantPast})
             
+            List(weeklyScores, id: \.key) { (week, scores) in
+                StudyWeeklyView(week: week, scores: scores)
+            }
+            .navigationTitle("Weekly Measurements")
+            .listStyle(.insetGrouped)
+        } else {
+            Text("No weekly scores have been transmitted so far")
+                .navigationTitle("Weekly Measurements")
+                .listStyle(.insetGrouped)
         }
-        .navigationTitle("Weekly Measurements")
-        .listStyle(.insetGrouped)
     }
     
-    private func weekString(score: StudyScore) -> String {
-        guard let week = score.week else {
+}
+
+private struct StudyWeeklyView: View {
+    
+    let week: Date?
+    let scores: [FetchedResults<StudyScore>.Element]
+    
+    var body: some View {
+        Section(header: Text(weekString(week))) {
+            ForEach(scores) { score in
+                Text("""
+                Country: \(score.country ?? "n/a")
+                Anomalous Cells: \(percentNumberFormatter.string(for: score.rateSuspicious) ?? "n/a")
+                Suspicious Cells: \(percentNumberFormatter.string(for: score.rateUntrusted) ?? "n/a")
+                """)
+            }
+        }
+    }
+    
+    
+    private func weekString(_ week: Date?) -> String {
+        guard let week = week else {
             return "n/a"
         }
         
         let sevenDaysBefore = week.addingTimeInterval(-60 * 60 * 24 * 7)
-        
         return mediumDateFormatter.string(from: sevenDaysBefore) + " - " + mediumDateFormatter.string(from: week)
     }
     
