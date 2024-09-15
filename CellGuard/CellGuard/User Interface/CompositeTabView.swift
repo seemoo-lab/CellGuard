6dc650f587f17f12379fc99878a0058c3e1000bf
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2024-09-15 16:49:27 +0200

    Fixes sysdiagnose import sheet not being showing iOS 14
    Yet another iOS 14 SwiftaUI bug :|

--- CellGuard/CellGuard/User Interface/CompositeTabView.swift
+++ CellGuard/CellGuard/User Interface/CompositeTabView.swift
@@ -21,7 +21,7 @@ private enum ShownTab: Identifiable {
 
 private enum ShownSheet: Hashable, Identifiable {
     case importFile(URL)
-
+    
     var id: Self {
         return self
     }
@@ -40,46 +40,51 @@ struct CompositeTabView: View {
     @State private var showingSheet: ShownSheet?
     
     var body: some View {
-        // If the introduction already was shown, we check on every start if we still have access to the local network
-        TabView(selection: $showingTab) {
-            SummaryTabView()
-                .tabItem {
-                    Label("Summary", systemImage: "shield.fill")
+        ZStack {
+            EmptyView()
+            // Multiple .sheet() & .fullScreenCover() statements on a single view are not supported in iOS 14
+            // See: https://stackoverflow.com/a/63181811
+            // See: https://www.hackingwithswift.com/forums/swiftui/using-sheet-and-fullscreencover-together/4258/13585
+                .sheet(item: $showingSheet) { (sheet: ShownSheet) in
+                    switch (sheet) {
+                    case let .importFile(url):
+                        NavigationView {
+                            ImportView(fileUrl: url)
+                        }
+                    }
                 }
-                .tag(ShownTab.summary)
-            MapTabView()
-                .tabItem {
-                    Label("Map", systemImage: "map.fill")
-                }
-                .tag(ShownTab.map)
-            PacketTabView()
-                .tabItem {
-                    Label("Packets", systemImage: "shippingbox")
-                }
-                .tag(ShownTab.packets)
+            
+            
+            TabView(selection: $showingTab) {
+                SummaryTabView()
+                    .tabItem {
+                        Label("Summary", systemImage: "shield.fill")
+                    }
+                    .tag(ShownTab.summary)
+                MapTabView()
+                    .tabItem {
+                        Label("Map", systemImage: "map.fill")
+                    }
+                    .tag(ShownTab.map)
+                PacketTabView()
+                    .tabItem {
+                        Label("Packets", systemImage: "shippingbox")
+                    }
+                    .tag(ShownTab.packets)
+            }
+            .fullScreenCover(isPresented: Binding(get: {
+                !introductionShown
+            }, set: { Bool in
+                // Ignore the change
+            })) {
+                IntroductionView()
+            }
         }
         .onAppear {
             // The tab bar on iOS 15 and above is by default translucent.
             // However in the map tab, it doesn't switch from the transparent to its opaque mode.
             // Therefore, we keep the tab for now always opaque.
             CGTabBarAppearance.opaque()
-        }
-        // Multiple .sheet() statements on a single view are not supported in iOS 14
-        // See: https://stackoverflow.com/a/63181811
-        .sheet(item: $showingSheet) { (sheet: ShownSheet) in
-            switch (sheet) {
-            case let .importFile(url):
-                NavigationView {
-                    ImportView(fileUrl: url)
-                }
-            }
-        }
-        .fullScreenCover(isPresented: Binding(get: {
-            !introductionShown
-        }, set: { Bool in
-            // Ignore the change
-        })) {
-            IntroductionView()
         }
         .onOpenURL { url in
             Self.logger.debug("Open URL: \(url)")
