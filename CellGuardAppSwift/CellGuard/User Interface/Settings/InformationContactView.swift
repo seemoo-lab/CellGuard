31e12ba33becba348bbdabe5d5baf12a8f152d5f
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2025-01-05 18:30:40 +0900

    Adds Max to the developers

--- CellGuardAppSwift/CellGuard/User Interface/Settings/InformationContactView.swift
+++ CellGuardAppSwift/CellGuard/User Interface/Settings/InformationContactView.swift
@@ -69,6 +69,7 @@ struct InformationContactView: View {
             Section(header: Text("Developers"), footer: Text("CellGuard is a research project by the Secure Mobile Networking Lab at TU Darmstadt (SEEMOO) and the Cybersecurity - Mobile & Wireless group at the Hasso Plattner Institute (HPI).")) {
                 KeyValueListRow(key: "Lukas Arnold", value: "SEEMOO")
                 KeyValueListRow(key: "Jiska Classen", value: "HPI")
+                KeyValueListRow(key: "Maximilian Paß", value: "HPI")
             }
             
             Section(header: Text("Acknowledgements")) {
