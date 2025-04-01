b15e8983109dcceaa9284869d40710529ce68b84
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2025-04-01 20:58:22 +0200

    Adapts OperatorDefinitions to new CSV country & operator format

--- CellGuardAppSwift/CellGuard/Cells/OperatorDefinitions.swift
+++ CellGuardAppSwift/CellGuard/Cells/OperatorDefinitions.swift
@@ -5,8 +5,60 @@
 //  Created by Lukas Arnold on 15.06.23.
 //
 
+import CSV
 import Foundation
 import OSLog
+
+struct NetworkCountry: Identifiable, Decodable {
+    let mcc: String
+    let name: String
+    let iso: String
+    let mncUrls: String?
+    
+    var id: String { self.mcc }
+    
+    enum CodingKeys: String, CodingKey {
+        case mcc
+        case name
+        case iso
+        case mncUrls = "mnc_urls"
+    }
+}
+
+enum NetworkOperatorStatus: Int, Decodable {
+    case notOperational = -1
+    case unknown = 0
+    case operational = 1
+}
+
+struct NetworkOperator: Decodable, Identifiable {
+    let mcc: String
+    let mnc: String
+    let status: NetworkOperatorStatus
+    
+    let brandName: String?
+    let brandUrl: String?
+    
+    let operatorName: String?
+    let operatorUrl: String?
+    
+    var id: String { self.mcc + "-" + self.mnc }
+    
+    enum CodingKeys: String, CodingKey {
+        case mcc
+        case mnc
+        case status
+        case brandName = "brand"
+        case brandUrl = "brand_url"
+        case operatorName = "operator"
+        case operatorUrl = "operator_url"
+    }
+}
+
+enum OperatorDefinitionsError: Error {
+    case invalidURL(String)
+    case cantConvertToString(String)
+}
 
 struct OperatorDefinitions {
     
@@ -15,83 +67,71 @@ struct OperatorDefinitions {
         category: String(describing: OperatorDefinitions.self)
     )
     
-    static let shared: OperatorDefinitions = {
-        // Get the URL of the qmi-definitions.json file in the bundle
-        guard let url = Bundle.main.url(forResource: "operator-definitions.json", withExtension: "gz") else {
-            logger.warning("Failed to get the URL for operator-definitions.json.gz")
-            return OperatorDefinitions(operators: [])
+    private static func csvReader(forResource resource: String) throws -> CSVReader {
+        // Get the URL of the countries.csv.gz file in the bundle
+        guard let url = Bundle.main.url(forResource: resource, withExtension: "gz") else {
+            throw OperatorDefinitionsError.invalidURL(resource)
         }
         
-        // Convert the file's content into Swift objects
-        do {
-            // Gunzip the compressed file
-            let data = try Data(contentsOf: url).gunzipped()
-            
-            // https://www.avanderlee.com/swift/json-parsing-decoding/
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let operators = try decoder.decode([NetworkOperator].self, from: data)
-            
-            return OperatorDefinitions(operators: operators)
-        } catch {
-            logger.warning("Failed to decode the JSON file operator-definitions.json.gz: \(error)")
-            return OperatorDefinitions(operators: [])
+        // Gunzip the compressed files
+        let data = try Data(contentsOf: url).gunzipped()
+        
+        // Convert to string
+        guard let str = String(data: data, encoding: .utf8) else {
+            throw OperatorDefinitionsError.cantConvertToString(resource)
         }
+        
+        // Create a CSV reader for the string
+        return try CSVReader(string: str, hasHeaderRow: true)
+    }
+    
+    static let shared: OperatorDefinitions = {
+        var countries: [NetworkCountry] = []
+        var operators: [NetworkOperator] = []
+        
+        // Convert the files contents into Swift objects
+        do {
+            let decoder = CSVRowDecoder()
+            
+            let readerCountries = try csvReader(forResource: "countries.csv")
+            while readerCountries.next() != nil {
+                countries.append(try decoder.decode(NetworkCountry.self, from: readerCountries))
+            }
+            
+            let readerOperators = try csvReader(forResource: "operators.csv")
+            while readerOperators.next() != nil {
+                operators.append(try decoder.decode(NetworkOperator.self, from: readerOperators))
+            }
+        } catch {
+            logger.warning("Failed to decode the CSV files countries.csv.gz & operators.csv.gz: \(error)")
+        }
+        
+        return OperatorDefinitions(countries: countries, operators: operators)
     }()
     
-    let countries: [Int: [Int: [NetworkOperator]]]
+    let countries: [Int: NetworkCountry]
+    let networks: [Int: [Int: [NetworkOperator]]]
     
-    private init(operators: [NetworkOperator]) {
+    private init(countries: [NetworkCountry], operators: [NetworkOperator]) {
         // We map the list of operators to a dictionary to allow for simple retrieval of operators by MCC & MNC
-        self.countries = Dictionary(grouping: operators) { $0.mcc }
-            .mapValues { Dictionary(grouping: $0) { $0.mnc } }
+        self.networks = Dictionary(grouping: operators) { Int($0.mcc) ?? -1 }
+            .mapValues { Dictionary(grouping: $0) { Int($0.mnc) ?? -1 } }
+        self.countries = Dictionary(uniqueKeysWithValues: countries.map( { (Int($0.mcc) ?? -1, $0) } ))
     }
     
+    // TODO: Refactor API usages
     func translate(country: Int32, iso: Bool = false) -> String? {
-        let firstNetworkOperator = countries[Int(country)]?.first?.value.first
-        return iso ? firstNetworkOperator?.countryIso : firstNetworkOperator?.countryName
+        let country = countries[Int(country)]
+        return iso ? country?.iso : country?.name
     }
     
+    // TODO: Refactor API usages
     func translate(country: Int32, network: Int32, iso: Bool = false) -> (String?, String?) {
-        if let networkOperator = networkOperator(country: country, network: network) {
-            return (iso ? networkOperator.countryIso: networkOperator.countryName, networkOperator.networkName)
-        }
+        let countryName = translate(country: country, iso: iso)
+        let operatorName = networks[Int(country)]?[Int(network)]?.first?.operatorName
         
-        // Fallback to a random network operator to return at least the country name
-        return (translate(country: country, iso: iso), nil)
-    }
-    
-    private func networkOperator(country: Int32, network: Int32) -> NetworkOperator? {
-        guard let countryDict = countries[Int(country)] else {
-            return nil
-        }
-        
-        guard let operators = countryDict[Int(network)] else {
-            return nil
-        }
-        
-        if operators.isEmpty {
-            return nil
-        }
-        
-        // We prefer entry with a given network name, especially if there are multiple entries for with the same MCC & MNC
-        if let networkOperator = operators.filter({ $0.networkName != nil }).first {
-            return networkOperator
-        }
-        
-        // Fallback to the first network operator, if there's none with a name
-        return operators.first
+        return (countryName, operatorName)
     }
     
 }
 
-struct NetworkOperator: Decodable, Identifiable {
-    let mcc: Int
-    let mnc: Int
-    let countryIso: String
-    let countryName: String
-    let countryCode: Int?
-    let networkName: String?
-    
-    var id: Int { self.mnc }
-}
