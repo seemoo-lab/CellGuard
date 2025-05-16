8feafea7b8a580bdf2a756c78738a915e6fd47a9
Lukas Arnold <lukas.arnold@stud.tu-darmstadt.de>
2025-05-16 14:19:26 +0200

    Create directory for token (if missing) and apply correct permissions for token.txt

--- RotateKeysDaemon/file.swift
+++ RotateKeysDaemon/file.swift
@@ -4,6 +4,20 @@ func writeWirelessToken(_ token: String) -> Bool {
     let pathString = "/var/wireless/Documents/CapturePacketsTweak/token.txt"
     let url = URL(fileURLWithPath: pathString)
     do {
+        // Create the directory (with permissions for _wireless user) if it does not exist
+        let fm = FileManager()
+        let parent = url.deletingLastPathComponent()
+        // TODO: Test
+        try fm.createDirectory(at: parent, withIntermediateDirectories: true, attributes: [
+            .ownerAccountName: "_wireless",
+            .groupOwnerAccountName: "_wireless",
+            .posixPermissions: 0o751 // rwxr-x--x
+        ])
+        // Write the token
+        let tokenData = Data(token.utf8)
+        fm.createFile(atPath: pathString, contents: tokenData, attributes: [
+            .posixPermissions: 0o660 // rw-rw----
+        ])
         try token.write(to: url, atomically: true, encoding: .utf8)
     } catch {
         print("Write Error -> \(error)")
